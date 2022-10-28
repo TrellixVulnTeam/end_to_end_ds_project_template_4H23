130fb1f835f4f602271513b543dd6666fb4c293e
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-28 22:03:11 +0000

    Adding tarfile member sanitization to extractall()

--- end_to_end_project/get_data.py
+++ end_to_end_project/get_data.py
@@ -33,7 +33,26 @@ def fetch_housing_data(data_url=HOUSING_URL, path=RAW_DATA_PATH):
 
     # Open, extract and close file
     with tarfile.open(file_path) as housing_tgz:
-        housing_tgz.extractall(path=path)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(housing_tgz, path=path)
 
 
 def load_housing_data(housing_path=RAW_DATA_PATH):
